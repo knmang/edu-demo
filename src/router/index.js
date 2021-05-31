7f3f45fd88ba615a7f8166aa81682659ac409bed
LAPTOP-LT6G7MQI\jcry <674436520@qq.com>
2021-05-31 18:36:57 +0800

    修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/login',
+    nane: 'login',
+    component: () => import(/* webpackChunName: 'login' */'@/views/login/index')
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/',
+    component: () => import('@/views/layout/index'),
+    children: [
+      {
+        path: '',
+        name: 'home'
+        // component: Home,
+      },
+      {
+        path: '/'
+      }
+    ]
+  },
+  {
+    path: '*',
+    name: 'error-page',
+    component: () => import('@/views/error-page/index')
   }
 ]
 
---
+++ src/views/layout/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+      <!-- 路由出口 -->
+      <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Layout'
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
